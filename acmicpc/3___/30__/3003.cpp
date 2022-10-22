0e5f779e5395665bce0a050cc0d6200791e8ff0a
sdnight5 <sdnight5@daum.net>
2022-10-22 13:35:02 +0900

    solved problems and cleaned folder
    acmicpc

---
+++ acmicpc/3___/30__/3003.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int k,q,r,b,n,p;
+
+    cin >> k >> q >> r >> b >> n >> p;
+    cout<< 1-k << " " 
+        << 1-q << " " 
+        << 2-r << " " 
+        << 2-b << " " 
+        << 2-n << " " 
+        << 8-p;
+    return 0;
+}
